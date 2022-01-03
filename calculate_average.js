8bf4b6e01ca657d27ade5bb56d695a9e71a9546c
AlainBox <93340821+AlainBox@users.noreply.github.com>
2022-01-03 01:16:15 +0100

    Create calculate_average.js

---
+++ calculate_average.js
@@ -0,0 +1,10 @@
+const calculateAverage = function(a, b, c, d, e){
+    return((a + b + c + d + e) / 5);
+};
+
+console.log(Math.round(calculateAverage(2, 4, 6, 8, 10)));
+console.log(Math.round(calculateAverage(2.6, 4.8, 6.3, 8.1, 10.9)));
+console.log(Math.round(calculateAverage(3.9, 45, 2, 19.6, 21)));
+console.log(Math.round(calculateAverage(2, 90.6, 50, 23.2, 13)));
+
+// This is a function that produces something.
